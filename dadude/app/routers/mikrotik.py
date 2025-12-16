3cf01ec95a58e52ba01e099198290654fcb92344
grandir66 <riccardo.grandi@gmail.com>
2025-12-16 19:33:52 +0100

    Fix: Replace remaining include_password with include_secrets

--- dadude/app/routers/mikrotik.py
+++ dadude/app/routers/mikrotik.py
@@ -201,7 +201,7 @@ async def backup_router_by_credential(
     import os
     
     customer_service = get_customer_service()
-    credential = customer_service.get_credential(credential_id, include_password=True)
+    credential = customer_service.get_credential(credential_id, include_secrets=True)
     
     if not credential:
         raise HTTPException(status_code=404, detail="Credenziale non trovata")
